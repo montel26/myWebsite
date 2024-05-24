e288ad92fdae3a583599f6b9ff84c916994dd2e0
montel26 <mwood023@student.wethinkcode.co.za>
2024-05-24 07:27:29 +0200

    add routing, side bar later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    SkillsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
