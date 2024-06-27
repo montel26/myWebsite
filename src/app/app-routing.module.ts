db63f3cb5c758809825b0ceac157b82fbb091063
montel26 <montelwood202@gmail.com>
2024-06-27 09:19:43 +0200

    side, about, home are complete .skills and contact are still being created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: "",
-    component : HomeComponent
+    component : ContactComponent
   }
 ];
 
