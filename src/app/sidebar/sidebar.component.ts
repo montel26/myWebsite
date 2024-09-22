ab6cdc4d65ff74552f9e47e82897dccd399c8871
montel26 <montelwood202@gmail.com>
2024-09-22 14:53:31 +0200

    fix tommorow

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { icon } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +11,22 @@ export class SidebarComponent {
 
   setActive(section: string): void{
     this.activeIcon = section;
+  
+
+  
+    // Remove 'active' class from all elements with class .bi or .email
+    const elements = document.querySelectorAll<HTMLElement>('.bi, .email');
+    elements.forEach(el => {
+      el.classList.remove('active');
+    });
+  
+    // Add 'active' class to the clicked element
+    const activeElement = document.querySelector<HTMLElement>(`[data-icon="${icon}"]`);
+    
+    if (activeElement) {
+      activeElement.classList.add('active');
+    }
   }
+  
 
 }
